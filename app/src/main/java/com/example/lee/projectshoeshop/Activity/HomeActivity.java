9e033de43f22ee796e34a2c74439c58212cfae29
John Doe <john@sample.com>
2018-07-18 18:19:39 +0700

    Update Home Menu

--- app/src/main/java/com/example/lee/projectshoeshop/Activity/HomeActivity.java
+++ app/src/main/java/com/example/lee/projectshoeshop/Activity/HomeActivity.java
@@ -1,5 +1,9 @@
 package com.example.lee.projectshoeshop.Activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,15 +16,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lee.projectshoeshop.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+import java.net.URL;
+
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
-
+    private TextView txtName;
+    private TextView txtEmail;
+    private ImageView avatar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +62,10 @@ public class HomeActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        avatar = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.imageView);
+        txtName = (TextView) navigationView.getHeaderView(0).findViewById(R.id.txtName1);
+       txtEmail = (TextView) navigationView.getHeaderView(0).findViewById(R.id.txtEmail1);
+
 
     }
 
@@ -55,6 +73,10 @@ public class HomeActivity extends AppCompatActivity
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+//        txtName.setText(currentUser.getDisplayName());
+//        txtEmail.setText(currentUser.getEmail());
+//        avatar.setImageURI(currentUser.getPhotoUrl());
+        updateUI(currentUser);
     }
 
     @Override
@@ -112,5 +134,27 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        if (user != null) {
+            txtName.setText(getString(R.string.google_status_fmt, user.getDisplayName()));
+           txtEmail.setText(getString(R.string.google_status_fmt, user.getEmail()));
+
+           try {
+               String url1 = user.getPhotoUrl().toString();
+               URL url = new URL(url1);
+               Bitmap image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+               avatar.setImageBitmap(image);
+           }catch (Exception e){
+               Toast.makeText(this, ""+e, Toast.LENGTH_SHORT).show();
+           }
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            finish();
+            startActivity(intent);
+
+        }
     }
 }
